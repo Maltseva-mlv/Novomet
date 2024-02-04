5749f8af0ade43413c5009a4be2003224674e1f1
Maltseva Lubov WORK GITHUB Profile <mlv.maltseva-1808@yandex.ru>
2024-02-04 13:38:53 +0500

    Добавлены клики по элементам

--- script.js
+++ script.js
@@ -6,74 +6,81 @@ const products = document.querySelectorAll('.details')
 const categories = document.querySelectorAll('.list__item');
 const categoryInfos = document.querySelectorAll('.product__info');
 
+
 function closeModal() {
     modal.classList.remove("open__modal");
 }
 
-// Работа с открытием вакансий
-vacancyBlocks.forEach(block => {
+if (vacancyBlocks.length > 0 && modal && modalInner && closeButton) {
+    vacancyBlocks.forEach(block => {
 
-	const title = block.getAttribute('data-title');
-	const location = block.getAttribute('data-location');
-	const time = block.getAttribute('data-time');
-
-	block.querySelector('.vacancy__name').textContent = title;
-	const ul = document.createElement('ul');
-	ul.className = 'vacancy__meta';
-    ul.innerHTML = `
-				<li>
-					<img src="../img/location.png" alt="" class="vacancy__meta--icon">
-					<span>${location}</span>
-				</li>
-				<li>
-					<img src="../img/time.png" alt="" class="vacancy__meta--icon">
-					<span>${time}</span>
-				</li>
-	`;
-
-	block.appendChild(ul);
-
-    block.addEventListener('click', function() {
-
-        const title = this.getAttribute('data-title');
-        const salary = this.getAttribute('data-salary');
-        const duties = this.getAttribute('data-duties').split(',');
-        const conditions = this.getAttribute('data-conditions').split(',');
-
-        modal.querySelector('.modal__title').textContent = title;
-        modal.querySelector('.salary').textContent = salary;
-        
-        const dutiesList = modal.querySelector('.desc__vacancy .duty:nth-child(1)'); 
-        dutiesList.innerHTML = '<h4 class="duty__title">Задачи: </h4>'; 
-        duties.forEach(duty => {
-            const li = document.createElement('li');
-            li.className = 'duty__item';
-            li.textContent = duty;
-            dutiesList.appendChild(li);
+        const title = block.getAttribute('data-title');
+        const location = block.getAttribute('data-location');
+        const time = block.getAttribute('data-time');
+    
+        block.querySelector('.vacancy__name').textContent = title;
+        const ul = document.createElement('ul');
+        ul.className = 'vacancy__meta';
+        ul.innerHTML = `
+                    <li>
+                        <img src="../img/location.png" alt="" class="vacancy__meta--icon">
+                        <span>${location}</span>
+                    </li>
+                    <li>
+                        <img src="../img/time.png" alt="" class="vacancy__meta--icon">
+                        <span>${time}</span>
+                    </li>
+        `;
+    
+        block.appendChild(ul);
+    
+        block.addEventListener('click', function() {
+    
+            const title = this.getAttribute('data-title');
+            const salary = this.getAttribute('data-salary');
+            const duties = this.getAttribute('data-duties').split(',');
+            const conditions = this.getAttribute('data-conditions').split(',');
+    
+            modal.querySelector('.modal__title').textContent = title;
+            modal.querySelector('.salary').textContent = salary;
+            
+            const dutiesList = modal.querySelector('.desc__vacancy .duty:nth-child(1)'); 
+            dutiesList.innerHTML = '<h4 class="duty__title">Задачи: </h4>'; 
+            duties.forEach(duty => {
+                const li = document.createElement('li');
+                li.className = 'duty__item';
+                li.textContent = duty;
+                dutiesList.appendChild(li);
+            });
+    
+            const conditionsList = modal.querySelector('.desc__vacancy .duty:nth-child(2)'); 
+            conditionsList.innerHTML = '<h4 class="duty__title">Условия: </h4>';
+            conditions.forEach(condition => {
+                const li = document.createElement('li');
+                li.className = 'duty__item';
+                li.textContent = condition;
+                conditionsList.appendChild(li);
+            });
+    
+            modal.classList.add("open__modal");
         });
+    });
 
-        const conditionsList = modal.querySelector('.desc__vacancy .duty:nth-child(2)'); 
-        conditionsList.innerHTML = '<h4 class="duty__title">Условия: </h4>';
-        conditions.forEach(condition => {
-            const li = document.createElement('li');
-            li.className = 'duty__item';
-            li.textContent = condition;
-            conditionsList.appendChild(li);
+}
+
+if (products.length > 0 && modal) {
+    products.forEach(product => {
+        product.addEventListener('click', function() {
+            modal.classList.add("open__modal");
+            console.log('ghb');
         });
+    })    
+}
 
-        modal.classList.add("open__modal");
-    });
-});
+if (closeButton) {
+    closeButton.addEventListener('click', closeModal);
+}
 
-products.forEach(product => {
-    product.addEventListener('click', function() {
-        modal.classList.add("open__modal");
-        console.log('ghb');
-    });
-})
-
-
-closeButton.addEventListener('click', closeModal);
 
 window.addEventListener('keydown', (e) => {
     if (e.key === "Escape") {
@@ -81,60 +88,99 @@ window.addEventListener('keydown', (e) => {
     }
 });
 
-modal.addEventListener('click', (event) => {
-    if (!modalInner.contains(event.target)) {
-        closeModal();
-    }
-});
-
-// Работа с переключением услуг
-
-function hideAllInfos() {
-    categoryInfos.forEach(function (info) {
-        info.style.display = 'none';
-    });
-}
-
- function showInfo(categoryId) {
-    var selectedInfo = document.getElementById(categoryId);
-
-    categoryInfos.forEach(function (info) {
-        if (info === selectedInfo) {
-            info.style.display = 'flex';
-        } else {
-            info.style.display = 'none';
+if (modal && modalInner) {
+    modal.addEventListener('click', (event) => {
+        if (!modalInner.contains(event.target)) {
+            closeModal();
         }
     });
 }
 
-const initialCategoryId = 'ending';
+document.querySelectorAll('.services__block').forEach(block => {
+    block.addEventListener('click', () => {
+        const category = block.getAttribute('data-category');
+        window.location.href = `pages/products.html#${category}`;
+    });
+});
 
-const initialCategoryItem = document.querySelector(`[data-category="${initialCategoryId}"]`);
-initialCategoryItem.classList.add('active__item');
+document.addEventListener("DOMContentLoaded", function() {
+    const hash = window.location.hash; 
+    hideAllInfos(); 
 
-// Скрываем все элементы категорий при загрузке страницы, кроме "Заканчивание скважин"
-categories.forEach(category => {
-    var categoryId = category.getAttribute('data-category');
-    if (categoryId !== initialCategoryId) {
-        hideAllInfos();
-        showInfo(initialCategoryId);
+    if (hash && document.querySelector(hash)) {
+        const activeCategoryItem = document.querySelector(`.list__item[data-category="${hash.substring(1)}"]`);
+        if (activeCategoryItem) {
+            setActiveCategory(activeCategoryItem);
+        }
+    } else {
+        setActiveCategory(initialCategoryItem); 
     }
 });
 
-// Показываем информацию при клике по категории
+function setActiveCategory(activeItem) {
+
+    categories.forEach(category => {
+        category.classList.remove('active__item');
+    });
+
+    activeItem.classList.add('active__item');
+    showInfo(activeItem.getAttribute('data-category'));
+}
+
 categories.forEach(category => {
     category.addEventListener('click', function (event) {
         event.preventDefault();
-
-        var categoryId = this.getAttribute('data-category');
-
-        hideAllInfos();
-        showInfo(categoryId);
-
-        categories.forEach(otherCategory => {
-            otherCategory.classList.remove('active__item');
-        });
-
-        this.classList.add('active__item');
+        setActiveCategory(this);
     });
 });
+
+
+if (categories.length > 0 && categoryInfos.length > 0) {
+    
+    categories.forEach(category => {
+        category.addEventListener('click', function (event) {
+            event.preventDefault();
+    
+            var categoryId = this.getAttribute('data-category');
+    
+            hideAllInfos();
+            showInfo(categoryId);
+    
+            categories.forEach(otherCategory => {
+                otherCategory.classList.remove('active__item');
+            });
+    
+            this.classList.add('active__item');
+        });
+    });
+    
+}
+
+function hideAllInfos() {
+    document.querySelectorAll('.product__info').forEach(info => {
+        info.style.display = 'none';
+    });
+}
+
+function showInfo(categoryId) {
+    const info = document.getElementById(categoryId);
+    if (info) {
+        info.style.display = 'flex';
+    }
+}
+
+
+const initialCategoryId = 'ending';
+const initialCategoryItem = document.querySelector(`[data-category="${initialCategoryId}"]`);
+
+if (initialCategoryItem && categories.length > 0) {
+initialCategoryItem.classList.add('active__item');
+
+    categories.forEach(category => {
+        var categoryId = category.getAttribute('data-category');
+        if (categoryId !== initialCategoryId) {
+            hideAllInfos();
+            showInfo(initialCategoryId);
+        }
+    });
+}
